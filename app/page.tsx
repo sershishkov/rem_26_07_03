a139066b9d8ba4fd00206ed5dc0062ecc0585c1c
sershishkov <ser.shishkov@gmail.com>
2026-07-04 16:02:01 +0300

    fixed react hydration error

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,90 @@
-import Image from "next/image";
+'use client';
+import { useState, useEffect } from 'react';
+
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+
+import { weCanDo } from '@/constants/description_weCanDo';
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setIsClient(true);
+  }, []);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    isClient && (
+      <Grid
+        container
+        rowSpacing={5}
+        sx={{
+          padding: '20px',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Grid size={12}>
+          <Typography variant='h3' align='center'>
+            Ремонт ОСББ
+          </Typography>
+          <Typography variant='h5' align='center'>
+            Наша компания осуществляет услуги по ремонтам для ОСББ, так же мы
+            работаем с физическими лицами
+          </Typography>
+        </Grid>
+
+        <Grid size={12}>
+          <Typography variant='h5' align='center'>
+            Основные наши направления:
+          </Typography>
+        </Grid>
+
+        {weCanDo.map((item) => (
+          <Grid key={item.workName} size={12}>
+            <Typography variant='h6' align='center'>
+              {item.workName}
+            </Typography>
+            <List>
+              <Grid
+                container
+                sx={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                {item.works.map((work) => (
+                  <Grid key={work} size={12}>
+                    <ListItem
+                      sx={{
+                        // border: '1px solid #ccc',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                      }}
+                    >
+                      <Typography
+                        variant='body2'
+                        align='center'
+                        sx={
+                          {
+                            // border: '1px solid red'
+                          }
+                        }
+                      >
+                        {work}
+                      </Typography>
+                    </ListItem>
+                  </Grid>
+                ))}
+              </Grid>
+            </List>
+          </Grid>
+        ))}
+      </Grid>
+    )
   );
 }
